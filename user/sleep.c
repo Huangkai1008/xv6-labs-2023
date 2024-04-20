b2bbe89f7b2a09b0f455243d113559543f6cbddb
huangkai <h1770360848@outlook.com>
2024-04-20 12:43:30 +0800

    feat(lab-util): implement a user-level sleep program for xv6

---
+++ user/sleep.c
@@ -0,0 +1,28 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+/**
+ * @brief Suspend execution for an interval of time
+ * @details The sleep command suspends execution for a minimum of seconds.
+ */
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(STDERR_FILENO, "Usage: sleep seconds...\n");
+        exit(1);
+    }
+
+    char *p = argv[1];
+    while (*p) {
+        if (!is_digit(*p)) {
+            fprintf(STDERR_FILENO, "%s is a invalid number\n", argv[1]);
+            exit(1);
+        }
+        p++;
+    }
+
+    p = argv[1];
+    int seconds = atoi(p);
+    sleep(seconds);
+    exit(0);
+}
